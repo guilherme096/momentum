039d4650106c0ac8996ba79dbcaddd758db27762
guilherme096 <guilherme.rosa60@gmail.com>
2024-04-15 13:23:26 +0100

    add home

--- src/pagesData.jsx
+++ src/pagesData.jsx
@@ -1,6 +1,12 @@
 import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
 
 const pagesData = [
+  {
+    path: "",
+    element: <Home />,
+    title: "home",
+  },
   {
     path: "login",
     element: <Login />,
