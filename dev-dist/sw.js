fc9a7b7e640620a928126c418d8d9011ff0f6b6e
guilherme096 <guilherme.rosa60@gmail.com>
2024-04-15 14:34:43 +0100

    add deploy workflow

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hg2r414gre"
+    "revision": "0.us7c6fv7cq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
