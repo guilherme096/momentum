a8f1ce0e96cb01bd3c3eebf5925963d379919366
ruimachado23 <rui.faria.machado@gmail.com>
2024-04-16 19:13:53 +0100

    add styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import './Navbar.css'; 
 
 export default function Navbar() {
     return (
         <div className="btm-nav">
             <CustomLink to="/" className="site-title">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    <path strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                 </svg>
             </CustomLink>
             <CustomLink to="/training-support">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 640 512" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="50" d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z"/>
+                    <path strokeWidth="50" d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z"/>
                 </svg>
             </CustomLink>
             <CustomLink to="/profile">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3a3 3 0 100 6 3 3 0 000-6zM12 12a5 5 0 00-5 5v1h10v-1a5 5 0 00-5-5z"/>
+                    <path strokeWidth="2" d="M12 3a3 3 0 100 6 3 3 0 000-6zM12 12a5 5 0 00-5 5v1h10v-1a5 5 0 00-5-5z"/>
                 </svg>
             </CustomLink>
         </div>
@@ -25,11 +28,14 @@ export default function Navbar() {
 function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to);
     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+    const scale = isActive ? { scale: 1.12} : { scale: 1, opacity: 0.7};
 
     return (
         <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>
-                {children}
+                <motion.div animate={scale} transition={{ duration: 0.3 }} >
+                    {children}
+                </motion.div>
             </Link>
         </li>        
     );
