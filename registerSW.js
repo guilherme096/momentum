842a31498670b9b0697664b7174ee8b7f203a971
guilherme096 <guilherme.rosa60@gmail.com>
2024-04-16 12:51:56 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/momentum/sw.js', { scope: '/momentum/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
