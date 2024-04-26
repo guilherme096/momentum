17b8a0a4b5f5dc2a2433c0286a23e208a85149a6
guilherme096 <guilherme.rosa60@gmail.com>
2024-04-26 17:48:38 +0100

    add cards to home page

--- src/layouts/CardLayout.jsx
+++ src/layouts/CardLayout.jsx
@@ -1,8 +1,9 @@
-export default function CardLayout({children, bgColor = "bg-neutral-200", width = "w-32", height = "h-32"}) {
-    const styling = `card rounded-lg m-2 max-${width} max-${height}  w-fit h-fit drop-shadow-lg ${bgColor} aspect-square`;
+export default function CardLayout({children, bgColor = "bg-neutral-200", width = "w-32", height = "h-auto", hasImage = false, image = ""}) {
+    const styling = `overflow-clip rounded-lg m-2 ${width} ${height} drop-shadow-lg ${bgColor} aspect-square`;
     return(
         <div className={styling}>
-            <div className="card-body">
+            {hasImage && <figure className="h-2/3 overflow-clip"><img className="object-cover h-full bg-cover" src={image} /></figure>}
+            <div className="card-body px-4 py-3">
                 {children} 
             </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,20 @@ import CardLayout from '../../layouts/CardLayout';
 import PageLayout from '../../layouts/PageLayout';
 import CrowdGraph from '../../components/CrowdGraph'
 import { Link } from "react-router-dom";
+import HomeCard from '../../components/HomeCard';
 
 
 export default function Home() {
     return (
         <PageLayout pageName="Home">
             <QRCard />
+            <div className="px-3 my-8 w-full h-40 flex justify-between items-center">
+                <HomeCard title="Class Schedule" image="https://images.unsplash.com/photo-1549995546-87cb41aa98a4?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/class-schedule"/>
+                <HomeCard title="Personal Trainer" image="https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/class-schedule"/>
+            </div>
+            <div className='px-3 mb-9 w-full h-40 flex'>
+                <HomeCard title="Training Zone" isSquare={false} image="https://images.unsplash.com/photo-1584466977773-e625c37cdd50?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/class-schedule"/>
+            </div >
             <CrowdGraph></CrowdGraph>
 
                 <Link to="/class-schedule">
