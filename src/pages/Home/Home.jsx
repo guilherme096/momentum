d5ea3a78fa4600abdc3c1ac40fced0458b5b6637
Henriqueee <henriqueft04@ua.pt>
2024-05-07 17:01:09 +0100

    client scheduling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,18 +14,14 @@ export default function Home() {
                 </div>
             </div>
             <div className="bg-transparent px-2 mt-6 w-full h-36 flex justify-around items-center">
-                <HomeCard title="Class Schedule" image="https://images.unsplash.com/photo-1549995546-87cb41aa98a4?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/class-schedule"/>
-                <HomeCard title="Personal Trainer" image="https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/class-schedule"/>
+                <HomeCard title="Class Schedule" link='/client-schedule-home' image="https://images.unsplash.com/photo-1549995546-87cb41aa98a4?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+                <HomeCard title="Personal Trainer" image="https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/personal-trainer"/>
             </div>
             <div className='bg-transparent px-2 mt-2 mb-9 w-full h-36 flex'>
-                <HomeCard title="Training Zone" isSquare={false} image="https://images.unsplash.com/photo-1584466977773-e625c37cdd50?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/class-schedule"/>
+                <HomeCard title="Training Zone" isSquare={false} image="https://images.unsplash.com/photo-1584466977773-e625c37cdd50?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" link="/start-train"/>
             </div >
             <CrowdGraph></CrowdGraph>
 
-                <Link to="/class-schedule">
-                    <button className='btn bg-primary'>class schedule</button>
-                </Link>
-            
 
         </PageLayout>
     );
