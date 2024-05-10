38bf92d4b039472e5c9c8931160f9b7300e95d11
ruimachado23 <rui.faria.machado@gmail.com>
2024-05-10 11:17:42 +0100

    bug fix - training page

--- src/components/EndTrainingModal.jsx
+++ src/components/EndTrainingModal.jsx
@@ -9,7 +9,7 @@ export default function ConfirmationModal() {
             <div className="modal-box">
                 <h1 className="font-bold text-xl">Are you sure?</h1>
                 <label className='flex items-center mt-6'>
-                        <input type="checkbox" className="checkbox mr-2" checked="true" />
+                        <input type="checkbox" className="checkbox mr-2" defaultChecked/>
                         <span>Save this training session</span>
                 </label>
                 <div className='flex flex-row items-end justify-end'>
