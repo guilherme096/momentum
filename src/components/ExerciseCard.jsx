0a6876cbc8bb193b17b034352052f2b48cec5f2e
guilherme096 <guilherme.rosa60@gmail.com>
2024-05-11 19:58:19 +0100

    reduce padding in exercise card

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,6 +1,6 @@
 export default function ExerciseCard({name, sets, reps, weight}) {
     return (
-        <div className='rounded-lg w-full h-fit shadow-md p-6 my-3 flex flex-col bg-base-200'>
+        <div className='rounded-lg w-full h-fit shadow-md p-5 my-3 flex flex-col bg-base-200'>
                <h3 className="text-xl font-bold mb-1">{name}</h3> 
                <div className='w-full flex flex-row justify-between'>
                    <div><p className="text-sm font-bold inline">Sets:</p><p className="text-lg inline ml-1">{sets}</p></div>
