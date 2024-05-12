ed039d452482a4293c68b27bad26016fcbc2c72f
guilherme096 <guilherme.rosa60@gmail.com>
2024-05-12 11:26:39 +0100

    logs with modal in calendar

--- src/info.js
+++ src/info.js
@@ -1,131 +1,138 @@
 export const classes = [
-  {
-    id: 1,
-    name: "CrossFit",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nunc, in eget amet. Eget amet, doloe sit ipsum elit. Adiposcing amet doloe ipsim consetetur.",
-    image: "https://via.placeholder.com/1",
-    date: "April 12th",
-    ocupation_math: 12 / 20,
-    ocupation: "12/20",
-    teacher: "Kevin G.",
-    time: "2pm - 3pm",
-  },
-  {
-    id: 2,
-    name: "Class 2",
-    description: "This is class 2",
-    image: "https://via.placeholder.com/200",
-    date: "",
-    ocupation_math: 0,
-    ocupation: "",
-    teacher: "",
-    time: "",
-  },
-  {
-    id: 3,
-    name: "Class 3",
-    description: "This is class 3",
-    image: "https://via.placeholder.com/250",
-    date: "",
-    ocupation_math: 0,
-    ocupation: "",
-    teacher: "",
-    time: "",
-  },
+    {
+        id: 1,
+        name: "CrossFit",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nunc, in eget amet. Eget amet, doloe sit ipsum elit. Adiposcing amet doloe ipsim consetetur.",
+        image: "https://via.placeholder.com/1",
+        date: "April 12th",
+        ocupation_math: 12 / 20,
+        ocupation: "12/20",
+        teacher: "Kevin G.",
+        time: "2pm - 3pm",
+    },
+    {
+        id: 2,
+        name: "Class 2",
+        description: "This is class 2",
+        image: "https://via.placeholder.com/200",
+        date: "",
+        ocupation_math: 0,
+        ocupation: "",
+        teacher: "",
+        time: "",
+    },
+    {
+        id: 3,
+        name: "Class 3",
+        description: "This is class 3",
+        image: "https://via.placeholder.com/250",
+        date: "",
+        ocupation_math: 0,
+        ocupation: "",
+        teacher: "",
+        time: "",
+    },
 ];
 
 export const exercises = [
-  {
-    id: 0,
-    name: "Incline Bench Press",
-    description:
-      "Incline Bench Press is a variation of the traditional Bench Press that targets the upper chest and some of the front shoulders.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    id: 1,
-    name: "Chest Press Machine",
-    description:
-      "The Chest Press Machine is a machine-based exercise that targets the chest.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    id: 2,
-    name: "Chest Fly",
-    description:
-      "The Chest Fly is an isolation exercise that targets the chest.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    id: 3,
-    name: "Lat Pulldown",
-    description:
-      "The Lat Pulldown is a machine-based exercise that targets the lats.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    id: 4,
-    name: "Row Machine",
-    description:
-      "The Row Machine is a machine-based exercise that targets the back.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    id: 5,
-    name: "Pull-up",
-    description: "The Pull-up is a bodyweight exercise that targets the back.",
-    image: "https://via.placeholder.com/150",
-  },
+    {
+        id: 0,
+        name: "Incline Bench Press",
+        description:
+            "Incline Bench Press is a variation of the traditional Bench Press that targets the upper chest and some of the front shoulders.",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 1,
+        name: "Chest Press Machine",
+        description:
+            "The Chest Press Machine is a machine-based exercise that targets the chest.",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 2,
+        name: "Chest Fly",
+        description:
+            "The Chest Fly is an isolation exercise that targets the chest.",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 3,
+        name: "Lat Pulldown",
+        description:
+            "The Lat Pulldown is a machine-based exercise that targets the lats.",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 4,
+        name: "Row Machine",
+        description:
+            "The Row Machine is a machine-based exercise that targets the back.",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 5,
+        name: "Pull-up",
+        description: "The Pull-up is a bodyweight exercise that targets the back.",
+        image: "https://via.placeholder.com/150",
+    },
+    {
+        id: 6,
+        name: "Push-up",
+        description: "The Push-up is a bodyweight exercise that targets the chest.",
+        image: "https://via.placeholder.com/150",
+    },
 ];
 
 export const exercise_logs = [
-  {
-    id: 1,
-    date: "2024-05-08",
-    exercises_done: [
-      {
+    {
         id: 1,
-        sets: 2,
-        reps: 8,
-        weight: 22.5,
-      },
-      {
+        date: "2024-05-08",
+        duration: "40m",
+        exercises_done: [
+            {
+                id: 1,
+                sets: 2,
+                reps: 8,
+                weight: 22.5,
+            },
+            {
+                id: 2,
+                sets: 2,
+                reps: 8,
+                weight: 25,
+            },
+            {
+                id: 3,
+                sets: 2,
+                reps: 8,
+                weight: 40,
+            },
+        ],
+    },
+    {
         id: 2,
-        sets: 2,
-        reps: 8,
-        weight: 25,
-      },
-      {
-        id: 3,
-        sets: 2,
-        reps: 8,
-        weight: 40,
-      },
-    ],
-  },
-  {
-    id: 2,
-    date: "2024-05-02",
-    exercises_done: [
-      {
-        id: 4,
-        sets: 2,
-        reps: 8,
-        weight: 22.5,
-      },
-      {
-        id: 5,
-        sets: 2,
-        reps: 8,
-        weight: 25,
-      },
-      {
-        id: 6,
-        sets: 2,
-        reps: 8,
-        weight: 40,
-      },
-    ],
-  },
+        date: "2024-05-02",
+        exercises_done: [
+            {
+                id: 4,
+                sets: 2,
+                reps: 8,
+                weight: 22.5,
+            },
+            {
+                id: 5,
+                sets: 2,
+                reps: 8,
+                weight: 25,
+            },
+            {
+                id: 6,
+                sets: 2,
+                reps: 8,
+                weight: 40,
+            },
+        ],
+    },
 ];
