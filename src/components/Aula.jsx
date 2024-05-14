0e7b171909a1acab0d3cae46da689fd00ce6cfc8
Henriqueee <henriqueft04@ua.pt>
2024-05-14 14:49:08 +0100

    add button

--- src/components/Aula.jsx
+++ src/components/Aula.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default function Aula({ index, startr, rowS, bgColor, aula, link }) {
   return (
@@ -12,11 +14,16 @@ export default function Aula({ index, startr, rowS, bgColor, aula, link }) {
       }}
       className={`border border-[#f2d3d8] rounded-lg p-2 m-2 flex items-center justify-start overflow-hidden`}
     >
-      <Link to={link} className="size-full">
-        <p className="font-semibold flex-auto min-w-0">{aula.name}</p>
-        <p className="shrink-0 pl-2 text-sm text-gray-600 whitespace-nowrap">
-          {aula.startTime} - {aula.endTime}
-        </p>
+      <Link to={link} className="size-full flex flex-row justify-between">
+        <div>
+          <p className="font-semibold flex-auto min-w-0">{aula.name}</p>
+          <p className="shrink-0 pl-2 text-sm text-gray-600 whitespace-nowrap">
+            {aula.startTime} - {aula.endTime}
+          </p>
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faPlusCircle} className="size-5 p-1" />
+        </div>
       </Link>
     </div>
   );
