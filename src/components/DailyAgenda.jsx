c7091dec32b5e86d41ee1da3ed72b17920fad42e
Henriqueee <henriqueft04@ua.pt>
2024-05-22 15:48:22 +0100

    delete button good

--- src/components/DailyAgenda.jsx
+++ src/components/DailyAgenda.jsx
@@ -20,18 +20,18 @@ export default function DailyAgenda({classes,link,addBookingCallback,}) {
   };
 
   const months = [
-    {id: 0, name: "January"},
-    {id: 1, name: "February"},
-    {id: 2, name: "March"},
-    {id: 3, name: "April"},
+    {id: 0, name: "Jan"},
+    {id: 1, name: "Feb"},
+    {id: 2, name: "Mar"},
+    {id: 3, name: "Apr"},
     {id: 4, name: "May"},
-    {id: 5, name: "June"},
-    {id: 6, name: "July"},
-    {id: 7, name: "August"},
-    {id: 8, name: "September"},
-    {id: 9, name: "October"},
-    {id: 10, name: "November"},
-    {id: 11, name: "December"},
+    {id: 5, name: "Jun"},
+    {id: 6, name: "Jul"},
+    {id: 7, name: "Aug"},
+    {id: 8, name: "Sep"},
+    {id: 9, name: "Oct"},
+    {id: 10, name: "Nov"},
+    {id: 11, name: "Dec"},
   ];
 
   const changeDay = (amount) => {
